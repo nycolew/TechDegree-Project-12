aa0e146558ed342301c978625772e63f81c6be18
Nycole Walsh <36087677+nycolew@users.noreply.github.com>
2019-04-11 00:09:31 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// Initialize carousel slider
+
+$(document).ready(function() {
+  $('.owl-carousel').owlCarousel({
+    items: 1,
+    stagePadding: 30,
+    margin: 4,
+    loop: true
+  });
+});
